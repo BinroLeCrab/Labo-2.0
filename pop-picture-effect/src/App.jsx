46f2ea2c7a00cf4e091bcf662fbc6f2677ee7809
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-02-19 16:43:41 +0100

    🚀 Start new effect

---
+++ pop-picture-effect/src/App.jsx
@@ -0,0 +1,14 @@
+import Noise from './components/Noise/Noise'
+import Content from './components/Content/Content'
+
+function App() {
+
+  return (
+    <main className='main'>
+      <Noise />
+      <Content />
+    </main>
+  )
+}
+
+export default App
