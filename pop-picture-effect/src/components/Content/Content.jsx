0fbca8f2f05e504e6a5f217936ae8ed9957f4dee
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-02-19 16:44:11 +0100

    🚧 Start main content

---
+++ pop-picture-effect/src/components/Content/Content.jsx
@@ -0,0 +1,13 @@
+import s from "./Content.module.scss";
+
+const Content= () => {
+
+    return (
+        <div className={s.content}>
+            <h1 className="hidden">MOISTURIZER</h1>
+            <img src="/assets/moist.png" alt="Moisturizer" className={s.moist} />
+        </div>
+    );
+};
+
+export default Content;
