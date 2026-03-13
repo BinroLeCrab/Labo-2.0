cedd59310fa2986acc06bd5f7025189f43852aaf
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-03-13 16:15:10 +0100

    🚀 Start new effect

---
+++ ascii-effect/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      {/* <p>ASCII effect</p> */}
+      <video src="assets/TURNSTILE.mp4" class="video" loop autoPlay muted>
+      </video>
+    </>
+  )
+}
+
+export default App
